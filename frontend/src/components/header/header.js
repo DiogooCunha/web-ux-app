c02f9ace0a572aa0cc307952153688e3446400a3
joaovieira007 <joaovitorvieira683@gmail.com>
2026-06-30 18:11:34 -0300

    fix: sidebar route to home

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -70,7 +70,7 @@ class HeaderComponent extends HTMLElement {
         nav.items = [
           {
             label: "Home",
-            href: "/",
+            href: "/web-ux-app/index.html",
             icon: `<svg viewBox="0 0 24 24" fill="currentColor"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>`,
           },
           {
